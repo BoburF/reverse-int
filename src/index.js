c714c584b2a30ced44953a36d07da25d0b197bb6
baxromxoja16 <changeitgo3@gmail.com>
2022-07-24 10:47:12 +0500

    fix: submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    // let str = String(n)
+    // console.log(str)
+    let arr = String(n).split("").reverse();
+
+    if (arr[arr.length - 1] == "-") {
+        arr.pop();
+        return arr.join("");
+    } else {
+        return arr.join("");
+    }
+
+    // let arr = str.split("").map((num)=>{
+    //     if(num !== '-'){
+    //         return Number(num)
+    //     }else if(num !== '-'){
+    //         // return
+    //     }
+    //   })
+    // console.log(arr)
+};
